41b1f21f5f55645f7ff9aeb99ae9b0b8ce8e4ccb
NathanCioppa <112644713+NathanCioppa@users.noreply.github.com>
2022-10-23 13:18:16 -0400

    aliens speed up
    aliens speed up as more get killed
    wont move down until live aliens reach the sides

--- index.js
+++ index.js
@@ -71,26 +71,29 @@ let alienX
 let alienY
 let moveAlien = 0
 let pose = 1
+let alienColor = 'green'
+let rightBound = 37
+let leftBound = 2
 
 function moveAliens(alienArea, alienWidth, distApart, distFromTop, alienArray, align, row) {
     alienY = dy
     alienX = 1 + moveAlien
-    if (countFrames % 100 === 0 && countFrames > 0) {
+    if (countFrames % alienSpeed === 0 && countFrames > 0) {
         moveAlien += d
         pose += 1
     }
 
-    if (alienX > 37 && d > 0) {
+    if (alienX > rightBound && d > 0) {
         d = -d
         dy += 10
     }
-        if (alienX < 2 && d < 0) {
+        if (alienX < leftBound && d < 0) {
             d = -d
             dy += 10
         }
 
     c.beginPath
-    c.fillStyle='green'
+    c.fillStyle=alienColor
     
     for (let ii = 0; ii < alienArea; ii++) {
         if (ii % alienWidth === 0 && ii > 0) {
@@ -107,9 +110,45 @@ function moveAliens(alienArea, alienWidth, distApart, distFromTop, alienArray, a
     c.stroke
 }
 
+function alienAnimations() {
+    if (pose % 2 !== 0) {
+        alienTop[15] = 41; alienTop[16] = 43; alienTop[17] = 44; alienTop[18] = 46
+        alienTop[20] = 48; alienTop[23] = 55
+        alienTop[24] = 57; alienTop[25] = 57; alienTop[28] = 62; alienTop[29] = 62
+
+        alienMiddle[9] = 11; alienMiddle[17] = 21
+        alienMiddle[18] = 22; alienMiddle[21] = 32
+        alienMiddle[22] = 33; alienMiddle[25] = 43
+        alienMiddle[26] = 56; alienMiddle[27] = 64
+        alienMiddle[36] = 78; alienMiddle[39] = 82; alienMiddle[40] = 82; alienMiddle[42] = 86;
+
+        alienBottom[16] = 61; alienBottom[19] = 70
+        alienBottom[23] = 75; alienBottom[26] = 80
+        alienBottom[28] = 84; alienBottom[29] = 85; alienBottom[34] = 94; alienBottom[35] = 95
+        
+    }
+    else if (pose % 2 === 0) {
+        alienTop[15] = 42; alienTop[16] = 51; alienTop[17] = 52; alienTop[18] = 45
+        alienTop[20] = 49; alienTop[23] = 54
+        alienTop[24] = 56; alienTop[25] = 58; alienTop[28] = 61; alienTop[29] = 63
+        
+        alienMiddle[9] = 66; alienMiddle[17] = 76
+        alienMiddle[18] = 55; alienMiddle[21] = 23
+        alienMiddle[22] = 55; alienMiddle[25] = 65
+        alienMiddle[26] = 55; alienMiddle[27] = 65
+        alienMiddle[36] = 80; alienMiddle[39] = 81; alienMiddle[40] = 83; alienMiddle[42] = 84;
+
+        alienBottom[16] = 62; alienBottom[19] = 69
+        alienBottom[23] = 73; alienBottom[26] = 82
+        alienBottom[28] = 86; alienBottom[29] = 87; alienBottom[34] = 92; alienBottom[35] = 93        
+    }
+}
+
 let topAliensLife = [1, 2, 3, 4, 5, 6]
 let midAliensLife = [1, 2, 3, 4, 5, 6]
 let bottomAliensLife = [1, 2, 3, 4, 5, 6]
+let alienSpeed = 50
+let kills = 0
 
 function hitAlien(row, distApart, width, align) {
     for (let i = 0; i <= 5; i++) {
@@ -118,6 +157,31 @@ function hitAlien(row, distApart, width, align) {
             shotX + Math.round(cols/2) <= moveAlien + (distApart * i) + width + align) {
                 hit = true
                 row[i] = 0
+                kills += 1
+                
+                if (kills > 0 && kills % 3 === 0 && kills <= 12) {
+                    alienSpeed -= 10
+                }
+                else if (kills > 12 && kills < 17) {
+                    alienSpeed -= 1
+                }
+                else if (kills === 17) {
+                    alienSpeed -= 4
+                    alienColor = 'red'
+                }
+
+                for (let i = 0; i <= 5; i++) {
+                    if (topAliensLife[i] !== 0 || midAliensLife[i] !== 0 || bottomAliensLife[i] !== 0) {
+                        leftBound = 2 - (i * 15)
+                        i = 7
+                    }
+                }
+                    for (let i = 5; i >= 0; i--) {
+                        if (topAliensLife[i] !== 0 || midAliensLife[i] !== 0 || bottomAliensLife[i] !== 0) {
+                            rightBound = 37 + (-(i - 5) * 15)
+                            i = -1
+                        }
+                    }
         }
     }
 }
@@ -238,44 +302,10 @@ function invadeSpace() {
         }
     drawPlayer(playerX)
 
-    if (pose % 2 !== 0) {
-        alienTop[15] = 41; alienTop[16] = 43; alienTop[17] = 44; alienTop[18] = 46
-        alienTop[20] = 48; alienTop[23] = 55
-        alienTop[24] = 57; alienTop[25] = 57; alienTop[28] = 62; alienTop[29] = 62
-        moveAliens(64, 8, 15.5, 0, alienTop, 0.25, topAliensLife)
-
-        alienMiddle[9] = 11; alienMiddle[17] = 21
-        alienMiddle[18] = 22; alienMiddle[21] = 32
-        alienMiddle[22] = 33; alienMiddle[25] = 43
-        alienMiddle[26] = 56; alienMiddle[27] = 64
-        alienMiddle[36] = 78; alienMiddle[39] = 82; alienMiddle[40] = 82; alienMiddle[42] = 86;
-        moveAliens(88, 11, 15, 10, alienMiddle, 0, midAliensLife)
-
-        alienBottom[16] = 61; alienBottom[19] = 70
-        alienBottom[23] = 75; alienBottom[26] = 80
-        alienBottom[28] = 84; alienBottom[29] = 85; alienBottom[34] = 94; alienBottom[35] = 95
-        moveAliens(96, 12, 15, 20, alienBottom, -0.5, bottomAliensLife)
-        
-    }
-    else if (pose % 2 === 0) {
-        alienTop[15] = 42; alienTop[16] = 51; alienTop[17] = 52; alienTop[18] = 45
-        alienTop[20] = 49; alienTop[23] = 54
-        alienTop[24] = 56; alienTop[25] = 58; alienTop[28] = 61; alienTop[29] = 63
-        moveAliens(64, 8, 15.5, 0, alienTop, 0.25, topAliensLife)
-        
-        alienMiddle[9] = 66; alienMiddle[17] = 76
-        alienMiddle[18] = 55; alienMiddle[21] = 23
-        alienMiddle[22] = 55; alienMiddle[25] = 65
-        alienMiddle[26] = 55; alienMiddle[27] = 65
-        alienMiddle[36] = 80; alienMiddle[39] = 81; alienMiddle[40] = 83; alienMiddle[42] = 84;
-        moveAliens(88, 11, 15, 10, alienMiddle, 0, midAliensLife)
-
-        alienBottom[16] = 62; alienBottom[19] = 69
-        alienBottom[23] = 73; alienBottom[26] = 82
-        alienBottom[28] = 86; alienBottom[29] = 87; alienBottom[34] = 92; alienBottom[35] = 93
-        moveAliens(96, 12, 15, 20, alienBottom, -0.5, bottomAliensLife)
-        
-    }
+    alienAnimations()
+    moveAliens(64, 8, 15.5, 0, alienTop, 0.25, topAliensLife)
+    moveAliens(88, 11, 15, 10, alienMiddle, 0, midAliensLife)
+    moveAliens(96, 12, 15, 20, alienBottom, -0.5, bottomAliensLife)
 
     c.beginPath
     c.fillStyle='orange'
@@ -346,7 +376,6 @@ function invadeSpace() {
                     hitAlien(bottomAliensLife, 15, 12, 0)
                 }
     }
-
     countFrames += 1
 }
 invadeSpace()
